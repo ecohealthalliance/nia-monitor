20a4d21a6276b6634da6861ae1a3032deee9a7be
Nathan Breit <nath@nathanathan.com>
2016-08-19 07:57:54 -0400

    Fix typo in API docs

--- public/.apidocs/api_project.js
+++ public/.apidocs/api_project.js
@@ -1,14 +1,14 @@
 define({
   "name": "Novel Infectious Agent Monitor API",
   "version": "0.0.2",
-  "description": "A datasource that provides information about infectious agents mentioned mentioned in online news feeds.",
+  "description": "A datasource that provides information about infectious agents mentioned in online news feeds - particularly ProMED-mail feeds.",
   "title": "NIAM API",
   "url": "http://niam.eha.io/api/",
   "sampleUrl": "http://niam.eha.io/api/",
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-08-18T11:16:36.405Z",
+    "time": "2016-08-19T11:56:57.765Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
