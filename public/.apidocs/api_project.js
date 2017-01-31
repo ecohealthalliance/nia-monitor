b28fa20f31c8a74f0fdb1ea2b5458b1c8bb53800
Nathan Breit <nath@nathanathan.com>
2017-01-31 04:06:10 -0500

    use https in api docs

--- public/.apidocs/api_project.js
+++ public/.apidocs/api_project.js
@@ -1,14 +1,14 @@
 define({
   "name": "Novel Infectious Agent Monitor API",
-  "version": "0.0.2",
+  "version": "0.0.3",
   "description": "A datasource that provides information about infectious agents mentioned in online news feeds - particularly ProMED-mail feeds.",
   "title": "NIAM API",
-  "url": "http://niam.eha.io/api/",
-  "sampleUrl": "http://niam.eha.io/api/",
+  "url": "https://niam.eha.io/api/",
+  "sampleUrl": "https://niam.eha.io/api/",
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-08-29T10:37:03.739Z",
+    "time": "2017-01-31T09:05:03.519Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
